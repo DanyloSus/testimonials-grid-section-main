5cec6360c7ea3ebe8235fe341108398c4a046760
Danylo Sus <danil.sushko19@gmail.com>
2023-08-19 14:08:31 +0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Testimonials grid section main
+
+from *www.frontendmentor.io*
+
+- [ ] Desktop Design
+- [ ] Responsive Design
+- [ ] Animations
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss')",.Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "moderate-violet": "hsl(263, 55%, 52%)",
+        "very-dark-grayish-blue": "hsl(217, 19%, 35%)",
+        "very-dark-blackish-blue": "hsl(219, 29%, 14%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
